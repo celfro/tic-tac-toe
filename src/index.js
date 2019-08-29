10deb0921a7bb0553790254f46a22a829b99bf22
Celia <celiafrom12@gmail.com>
2019-08-29 12:09:16 +0100

    feat: render method that adds number in each square

--- src/index.js
+++ src/index.js
@@ -11,30 +11,66 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-class Board extends React.Component {
-  conxtructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
-      />;
-    )
-  }
-}
-
 class Square extends React.Component {
   render() {
-    return(
-      <button className="square" onClick={function() { alert('click'); }}>
-      {this.props.value}
+    return (
+      <button className="square">
+        {this.props.value}
       </button>
     );
   }
 }
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
